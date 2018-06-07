d2f13ffc1f8dd6f17e7245a6a405e2ecd77b6399
Willon <willonnava@gmail.com>
2018-06-06 23:11:18 -0300

    Finalização do script js

--- assets/js/dft_exprod.js
+++ assets/js/dft_exprod.js
@@ -1,15 +1,19 @@
 jQuery(function($) {
     
-    // aplicar o select2
     $("#campo-exclusivo").select2();
     
-    
-    // Esconder a categoria Personalizado
     $('#product_catchecklist li > label').each(function() {
-        
-        if ($(this).text() === ' Personalizado' ) {
-            //$(this).hide();
-        }
+        if ( $(this).text() === ' Personalizado' )
+            $(this).addClass("checkbox-personalizado").hide();
     });
+
+    $("#campo-exclusivo").change(autoCheckCategory);
+
+    function autoCheckCategory() {
+        if ( $(this).val() === "" )
+            $(".checkbox-personalizado input").prop("checked", false);
+        else 
+            $(".checkbox-personalizado input").prop("checked", true);
+    }
     
 });
